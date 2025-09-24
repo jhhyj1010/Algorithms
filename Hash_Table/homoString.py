2aa29f04a9ccd449ed060ba049b55513e5902801
Ji Hui <273301130@qq.com>
2025-09-24 15:45:04 +0800

    add homestring.py

---
+++ Hash_Table/homoString.py
@@ -0,0 +1,44 @@
+"""
+给定两个字符串 s 和 t ，判断它们是否是同构的。
+如果 s 中的字符可以按某种映射关系替换得到 t ，那么这两个字符串是同构的。
+每个出现的字符都应当映射到另一个字符，同时不改变字符的顺序。不同字符不能映射到同一个字符上，相同字符只能映射到同一个字符上，字符可以映射到自己本身。
+
+示例 1:
+输入：s = "egg", t = "add"
+输出：true
+
+示例 2：
+输入：s = "foo", t = "bar"
+输出：false
+
+示例 3：
+输入：s = "paper", t = "title"
+输出：true
+
+提示：
+1 <= s.length <= 5 * 104
+t.length == s.length
+s 和 t 由任意有效的 ASCII 字符组成
+"""
+
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        '''if len(s) != len(t):
+            return False
+        c_mapping = {}
+        for i in range(len(s)):
+            if s[i] not in c_mapping:
+                c_mapping[s[i]] = t[i]
+            else:
+                if c_mapping[s[i]] != t[i]:
+                    return False
+        return True'''
+        if [s.index(c) for c in s] == [t.index(c) for c in t]:
+            return True
+        return False
+
+# Test 
+s = "egg"
+t = "add"
+#assert(Solution().isIsomorphic(s, t) == True)
