d7d3bed18058543a90eb4f45cf4e5c6519d2de42
Ji Hui <273301130@qq.com>
2025-09-25 13:27:16 +0800

    add groupAnagrams

---
+++ Hash_Table/groupAnagrams.py
@@ -0,0 +1,39 @@
+"""
+给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+
+示例 1:
+输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+解释：
+在 strs 中没有字符串可以通过重新排列来形成 "bat"。
+字符串 "nat" 和 "tan" 是字母异位词，因为它们可以重新排列以形成彼此。
+字符串 "ate" ，"eat" 和 "tea" 是字母异位词，因为它们可以重新排列以形成彼此。
+
+示例 2:
+输入: strs = [""]
+输出: [[""]]
+
+示例 3:
+输入: strs = ["a"]
+输出: [["a"]]
+
+提示：
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] 仅包含小写字母
+"""
+
+from collections import defaultdict
+import typing
+
+class Solution:
+    def groupAnagrams(self, strs: str) -> typing.List[typing.List[str]]:
+        results = defaultdict(list)
+        for w in strs:
+            results[''.join(sorted(w))].append(w)
+        return list(results.values())
+
+
+# Test
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+print(Solution().groupAnagrams(strs=strs))
